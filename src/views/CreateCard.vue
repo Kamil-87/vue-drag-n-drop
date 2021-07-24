ae0501813531f237d7e4063bafbae98684dcb5ab
kamil-87 <k.kamil87-87@yandex.ru>
2021-07-24 10:39:17 +0500

    adding html-code and routing

--- src/views/CreateCard.vue
+++ src/views/CreateCard.vue
@@ -1,5 +1,68 @@
 <template>
-  <div>
-    <h1>This is an CreateCard page</h1>
+  <div class="container">
+    <h1 class="pt-1">Добавить новую задачу</h1>
+
+    <form class="form mb-2" @submit.prevent="submitHandler">
+      <div class="form-control">
+        <input
+          type="text"
+          id="name"
+          required
+          v-model="name"
+        >
+        <span class="bar"></span>
+        <label for="name">Название</label>
+        <small>Поле обязательно</small>
+      </div>
+
+      <div class="form-control">
+        <textarea
+          name="description"
+          id="description"
+          required
+          v-model="description"
+        ></textarea>
+        <span class="bar"></span>
+        <label for="description">Описание</label>
+      </div>
+
+      <button
+        class="btn primary"
+        type="submit"
+      >
+        Добавить
+      </button>
+      <button
+        class="btn"
+        @click="cancelHandler"
+      >
+        Отмена
+      </button>
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      description: ''
+    }
+  },
+  methods: {
+    cancelHandler() {
+      this.$router.push('/')
+    },
+    submitHandler() {
+      const formData = {
+        id: Date.now(),
+        name: this.name,
+        description: this.description
+      }
+      console.log(formData)
+      // this.$router.push({name: 'Home'})
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,131 @@
 <template>
-  <div>
-    <h1>This is an Home page</h1>
-  </div>
+  <main class="container">
+    <header class="header pt-1">
+      <h1 class="mb-2">Drag And Drop</h1>
+
+      <div class="action-block">
+        <button
+          class="btn primary"
+          @click="createCard"
+        >Добавить
+        </button>
+
+        <div class="form-checkbox">
+          <label for="checkbox">click&drag</label>
+          <input
+            type="checkbox"
+            id="checkbox"
+            class="checkbox"
+          >
+        </div>
+      </div>
+    </header>
+
+    <hr>
+
+    <div class="dashboard">
+      <div
+        class="col"
+        v-for="col in columns"
+        :key="col.id"
+      >
+        <div
+          class="card"
+          v-for="card in cards"
+          :key="card.id"
+        >
+          <h3>{{ card.name }}</h3>
+          <p>{{ card.description }}</p>
+        </div>
+      </div>
+    </div>
+
+
+  </main>
 </template>
 
 <script>
+const initCards = [
+    {
+      id: 1,
+      title: 'Name',
+      description: 'description',
+      categoryId: 1
+    },
+    {
+      id: 2,
+      title: 'Name',
+      description: 'description',
+      categoryId: 2
+    },
+  ],
+  initColumns = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4},
+  ]
+
 
 export default {
   name: 'Home',
-  components: {}
+  components: {},
+  data() {
+    return {
+      columns: [],
+      cards: []
+    }
+  },
+  methods: {
+    createCard() {
+      this.$router.push('/create-card')
+    }
+  },
+  mounted() {
+    this.columns = [...initColumns]
+    this.cards = [...initCards]
+  }
 }
 </script>
+
+<style scoped>
+.action-block {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.dashboard {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.col {
+  display: flex;
+  flex-direction: column;
+  flex: 0 0 24%;
+
+  border-radius: 5px;
+  padding: .5rem 1rem;
+  overflow: hidden;
+  border: 1px dotted #2c3e50;
+}
+
+.card {
+  overflow: hidden;
+  height: 90px;
+  padding: 1rem;
+  margin-bottom: .5rem;
+  border-radius: 5px;
+  box-shadow: 2px 3px 10px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  cursor: move;
+  user-select: none;
+}
+.card p, .card h3 {
+  overflow: hidden;
+}
+
+</style>
